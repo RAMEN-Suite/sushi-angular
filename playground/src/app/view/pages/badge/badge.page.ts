8d42815432acf2597859b7daeba095143c6e8ba8
Sebastian Enns <sebastian.enns@mni.thm.de>
2026-07-06 19:05:20 +0200

    refactor(badge): Changed badge page examples.

--- playground/src/app/view/pages/badge/badge.page.ts
+++ playground/src/app/view/pages/badge/badge.page.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { SuiBadge } from '@ramen-suite/sushi';
+import { SuiBadge, SuiButton } from '@ramen-suite/sushi';
 
 @Component({
   selector: 'app-badge-page',
-  imports: [SuiBadge],
+  imports: [SuiBadge, SuiButton],
   templateUrl: './badge.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
