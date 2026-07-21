6c00127f852414f3cbef6f4bb2355ff36a415160
Sebastian Enns <sebastian.enns@mni.thm.de>
2026-07-21 21:13:31 +0200

    feat(avatar): Added combined avatar with masks and shapes.

---
+++ sushi/src/lib/avatar/avatar.interfaces.ts
@@ -0,0 +1,23 @@
+export type AvatarStatus = 'online' | 'offline';
+export type AvatarSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type AvatarMaskHalf = 'first' | 'second';
+
+export type AvatarShape =
+  | 'square'
+  | 'rounded'
+  | 'circle'
+  | 'squircle'
+  | 'heart'
+  | 'hexagon'
+  | 'hexagon-2'
+  | 'decagon'
+  | 'pentagon'
+  | 'diamond'
+  | 'mask-square'
+  | 'mask-circle'
+  | 'star'
+  | 'star-2'
+  | 'triangle'
+  | 'triangle-2'
+  | 'triangle-3'
+  | 'triangle-4';
---
+++ sushi/src/lib/avatar/index.ts
@@ -0,0 +1,2 @@
+export * from './avatar.directive';
+export * from './avatar.interfaces';
