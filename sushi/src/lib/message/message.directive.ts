eef8bae2e89c470af43a3de164740c6883fa8232
Sebastian Enns <sebastian.enns@mni.thm.de>
2026-07-21 21:19:05 +0200

    feat(message): Added alert messages.

---
+++ sushi/src/lib/message/message.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, input, InputSignal } from '@angular/core';
+import { MessageOrientation, MessageSeverity, MessageVariant } from './message.interfaces';
+
+@Directive({
+  selector: '[suiMessage]',
+  standalone: true,
+  host: {
+    class: 'alert sui-message',
+    '[class.alert-info]': 'severity() === "info"',
+    '[class.alert-success]': 'severity() === "success"',
+    '[class.alert-warning]': 'severity() === "warning"',
+    '[class.alert-error]': 'severity() === "error"',
+
+    '[class.alert-soft]': 'variant() === "soft"',
+    '[class.alert-outline]': 'variant() === "outlined"',
+    '[class.alert-dash]': 'variant() === "dash"',
+
+    '[class.alert-horizontal]': 'orientation() === "horizontal"',
+    '[class.alert-vertical]': 'orientation() === "vertical"',
+  },
+})
+export class Message {
+  public readonly severity: InputSignal<MessageSeverity | null> = input<MessageSeverity | null>(null);
+  public readonly variant: InputSignal<MessageVariant | null> = input<MessageVariant | null>(null);
+  public readonly orientation: InputSignal<MessageOrientation | null> = input<MessageOrientation | null>(null);
+}
---
+++ sushi/src/lib/message/message.interfaces.ts
@@ -0,0 +1,3 @@
+export type MessageSeverity = 'info' | 'success' | 'warning' | 'error';
+export type MessageVariant = 'soft' | 'outlined' | 'dash';
+export type MessageOrientation = 'horizontal' | 'vertical';
